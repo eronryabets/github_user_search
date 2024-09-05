6239a986c62bf4e097ec06a8bd2b42151c90851d
eronryabets <eronryabets@gmail.com>
2024-09-05 16:30:57 +0300

    Theme switcher

--- templates/default/TemplateName.tsx
+++ templates/default/TemplateName.tsx
@@ -1,9 +1,11 @@
+// @ts-ignore
 import styles from './TemplateName.module.scss';
 
 interface TemplateNameProps { }
 
 export const TemplateName = ({ }: TemplateNameProps) => (
-  <div className={styles.TemplateName} data-testid="TemplateName">
+  // <div className={styles.TemplateName} data-testid="TemplateName">
+  <div className={styles.templateName}>
     TemplateName Component
   </div>
 );
