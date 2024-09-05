88ec20debfd0875d643db392f9eba88708144de0
eronryabets <eronryabets@gmail.com>
2024-09-05 18:15:33 +0300

    update Search panel

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,50 +1,49 @@
-import {ReactComponent as SearchIcon} from 'assets/icon-search.svg';
-import {Button} from 'components/Button';
+import { ReactComponent as SearchIcon } from 'assets/icon-search.svg';
+import { Button } from 'components/Button';
 
 import styles from './Search.module.scss';
-import React, {useRef} from "react";
+import React from "react";
 
 interface SearchProps {
-    hasError: boolean,
-    onSubmit: (text: string) => void,
+  hasError: boolean,
+  onSubmit: (text: string) => void,
 }
 
-export const Search = ({hasError, onSubmit}: SearchProps) => {
+type FormFields = {
+  username: HTMLInputElement,
+}
 
-    const searchRef = useRef<HTMLInputElement | null>(null);
-    const handleSubmit = (event: React.FormEvent) => {
-        event.preventDefault();
-        const text = searchRef.current ? searchRef.current.value : '';
+export const Search = ({ hasError, onSubmit }: SearchProps) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement & FormFields>) => {
+    event.preventDefault();
+    const text = event.currentTarget.username.value;
 
-        if (text) {
-            onSubmit(text);
-            if (searchRef.current) {
-                searchRef.current.value = '';
-            }
-        }
+    if (text.trim()) {
+      onSubmit(text);
+      event.currentTarget.reset();
     }
+  }
 
-    return (
-        <form onSubmit={handleSubmit} autoComplete="off">
-            <div className={styles.search}>
-                <label htmlFor="search" className={styles.label}>
-                    <SearchIcon/>
-                </label>
-                <input
-                    ref={searchRef}
-                    type="text"
-                    className={styles.textField}
-                    id="search"
-                    name="username"
-                    placeholder="Search GitHub username..."
-                />
-                {hasError && (
-                    <div className={styles.error}>
-                        No result
-                    </div>
-                )}
-                <Button>Search</Button>
-            </div>
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit} autoComplete="off">
+      <div className={styles.search}>
+        <label htmlFor="search" className={styles.label}>
+          <SearchIcon />
+        </label>
+        <input
+          type="text"
+          className={styles.textField}
+          id="search"
+          name="username"
+          placeholder="Search GitHub username..."
+        />
+        {hasError && (
+          <div className={styles.error}>
+            No result
+          </div>
+        )}
+        <Button>Search</Button>
+      </div>
+    </form>
+  );
 };
